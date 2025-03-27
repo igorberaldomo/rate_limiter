03b5829f145af2584a1591a29143d42bf9a0b304
Igor <igorberaldomo@gmai.com>
2025-03-27 16:19:40 -0300

    implementando token

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"net"
 	"net/http"
-	"time"
-	"encoding/json"
 	"sync"
-	"golang.org/x/time/rate"
+	"time"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+	"golang.org/x/time/rate"
 )
 
 func main() {
-	
-	http.Handle("/", perClientLimiter(handler))
+	r :=chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/", perClientLimiter(handler))
+	r.Get("/{API_KEY}", perTokenLimiter(handler))
 	
 	http.ListenAndServe(":8080", nil)
 	
@@ -67,6 +72,53 @@ func perClientLimiter(next func(writer http.ResponseWriter, request *http.Reques
 		}
 		clients[ip].lastCalled = time.Now()
 		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+			w.WriteHeader(http.StatusTooManyRequests)
+			message := message{
+				Status: "error",
+				Body:   "Too many requests",
+			}
+			json.NewEncoder(w).Encode(message)
+			return
+		}
+		mu.Unlock()
+		next(w, r)
+	})
+}
+
+func perTokenLimiter(next func(writer http.ResponseWriter, request *http.Request)) http.Handler {
+	auth := chi.URLParam(next, "API_KEY")
+	type token struct {
+		limiter *rate.Limiter
+		lastCalled    time.Time
+	}
+
+	var (
+		mu   sync.Mutex
+		tokens = make(map[string]*token)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for auth, c := range tokens {
+				if time.Since(c.lastCalled) > 5*time.Minute {
+					delete(tokens, auth)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+
+		mu.Lock()
+		if _ , found :=tokens[auth]; !found {
+			tokens[auth] = &token{limiter: rate.NewLimiter(rate.Every(time.Second), 100)}
+		}
+		tokens[auth].lastCalled = time.Now()
+		if !tokens[auth].limiter.Allow() {
 			mu.Unlock()
 			w.WriteHeader(http.StatusTooManyRequests)
 			message := message{
